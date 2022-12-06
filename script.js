d76c0b270f11ce3bbfd04328b137c4ef49b94392
Lagxy <justtrashcount@gmail.com>
2022-12-06 20:29:40 +0700

    ready upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleDetail (e){
+function toggleDetail(e){
     const target = $(e.target);
 
     if($(target).hasClass("active")) {
@@ -11,3 +11,19 @@ function toggleDetail (e){
     const detail = $(item).children(".about-exp-items-detail");
     $(detail).slideToggle();
 } 
+
+function onFormSubmit(e){
+    e.preventDefault();
+    const subject = $("#InputSubject");
+    const massage = $("#InputMassage");
+
+    if(!$(subject).val()) {
+        alert("Subject is require");
+    } else if(!$(massage).val()) {
+        alert("Please enter your massage");
+    } else {
+        alert("form is submited!");
+        $(subject).val("");
+        $(massage).val("");
+    }
+}
